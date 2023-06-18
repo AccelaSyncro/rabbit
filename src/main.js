a065fc581c79365cab4bfee4a33634c9caec5c1e
Accelasyncro <3228615925@qq.com>
2023-06-18 22:41:21 +0800

    product区域常规渲染

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import router from './router'
 
 //引入初始化样式文件
 import '@/styles/common.scss'
+import { lazyPlugin } from './directives'
+import { autocompleteProps } from 'element-plus'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
